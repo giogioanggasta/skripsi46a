a7ce7878b62aed8f7ce16109348fbdfd1dedc8e3
giogioanggasta <giogioanggasta@gmail.com>
2023-05-22 18:40:17 +0700

    Tambahan

--- view/Data.php
+++ view/Data.php
@@ -1,157 +1,11 @@
-<?php include('../model/Model-DataTransaksi.php') ?>
-<?php include('../model/SendMail.php') ?>
-
-
-<!DOCTYPE html>
-
-
-<head>
-<title>Data Transaksi</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
-  <style>
-    @font-face {
-      font-family: header;
-      src: url("../fonts/Ailerons-Typeface.otf");
-    }
-
-    @font-face {
-      font-family: texts;
-      src: url("../fonts/Renner_ 400 Book.ttf");
-    }
-
-    @font-face {
-      font-family: navBarFont;
-      src: url("../fonts/Kiona-Regular.ttf");
-      font-style: bold;
-    }
-
-    h1 {
-      font-family: header;
-      font-size: 70px;
-      color: #373737;
-    }
-
-    a {
-      font-family: navBarFont;
-      font-size: 25px;
-      color: #868B8E;
-      font-style: bold;
-    }
-
-
-    h2 {
-      font-family: navBarFont;
-      font-size: 30px;
-      color: white;
-      margin-top: 40px;
-      margin-bottom: 40px;
-    }
-
-    h5 {
-      font-family: navBarFont;
-      font-size: 20px;
-      color: white;
-    }
-
-    .enterbutton {
-      width: 20%;
-      background-color: grey;
-      border: none;
-      color: white;
-      padding: 14px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      margin: 8px 0;
-      margin-left: 75%;
-      margin-top: 5%;
-      cursor: pointer;
-      border-radius: 4px;
-      transition: 0.5s;
-    }
-
-    .button {
-      width: 82.5%;
-      background-color: white;
-      border: 1px solid #ccc;
-      color: white;
-      padding: 14px 20px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      margin: 8px 0;
-      cursor: pointer;
-      border-radius: 4px;
-      transition: 0.5s;
-    }
-
-
-    .button:hover {
-      background-color: grey;
-      transition: 0.5s;
-    }
-
-    h5 {
-      font-family: navBarFont;
-      font-size: 20px;
-      color: white;
-    }
-
-    table {
-      width: 70%;
-      color: black;
-      margin-left: 10%;
-      margin-top: 2.5%;
-    }
-
-    table,
-    tr,
-    td {
-      padding: 10px;
-    }
-
-    input[type=submit] {
-      width: 20%;
-      background-color: grey;
-      color: white;
-      padding: 14px 20px;
-      margin: 8px 0;
-      margin-top: 5%;
-      margin-left: 55%;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-    }
-
-    .tablecust {
-      font-family: texts2;
-      font-size: 15px;
-      color: #373737;
-    }
-  </style>
-</head>
-
-<body>
-
-
-
-    <div class="w3-bar w3-white w3-border " id="menu" style="background-color: #11355b">
-        <a href="HomeAdmin.php" class="w3-bar-item"><img src="../images/logo46a.png" style="width:150px"></a>
-        <a href="Data.php" class="w3-bar-item" style="float: right; margin-right: 2.5%; margin-top:4%; text-decoration: none; color: white">Transaksi</a>
-        <a href="DataPerpanjang.php" class="w3-bar-item" style="float: right; margin-right: 2.5%; margin-top:4%; text-decoration: none; color: white">Perpanjang</a>
-        <a href="DataPelanggan.php" class="w3-bar-item" style="float: right; margin-right: 2.5%; margin-top:4%; text-decoration: none; color: white">Pelanggan</a>
-      </div>
-      
-
-      <br><br>
+<?php
+include('../helper/flash_session.php');
+include('../model/kelolaModel.php');
+$title = "Laporan Tipe Kamar";
+include('tmpadmin/header.php');
+include('tmpadmin/nav-data.php');
+// include('../model/adminModel.php');
+?>
 
   <a class="w3-display-middle" style="color:black;float: center; margin-top: -13%; margin-left:45%; text-decoration: none; font-size: 120%">Tabel Transaksi</a>
 
@@ -190,13 +44,7 @@
       </thead>
 
 
-      <?php
-      $sql = "SELECT * FROM transaksi ";
-      $results = mysqli_query($db, $sql) or die(mysqli_error($db));
-
-      while ($row = mysqli_fetch_array($results)) {
-
-      ?>
+     
         <form action="DataTransaksi.php" method="post">
           <tr>
             <td><?php echo $row["idTransaksi"] ?></td>
@@ -254,10 +102,7 @@
       </div>
         </form>
        
-      <?php
-
-      }
-      ?>
+      
     </table>
   </div>
 
