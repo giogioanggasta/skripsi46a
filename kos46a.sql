54c10bbe02481728bf43c37bce385ed43bd86c83
satriags <satriagirisyawalludin@gmail.com>
2023-05-22 21:21:49 +0700

    db new

--- kos46a.sql
+++ kos46a.sql
@@ -1,254 +1,180 @@
--- phpMyAdmin SQL Dump
--- version 4.9.11
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3307
--- Waktu pembuatan: 19 Bulan Mei 2023 pada 16.04
--- Versi server: 10.11.0-MariaDB
--- Versi PHP: 5.6.40
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : localwest_mariadb
+ Source Server Type    : MariaDB
+ Source Server Version : 101100
+ Source Host           : localhost:3307
+ Source Schema         : kos46a
 
+ Target Server Type    : MariaDB
+ Target Server Version : 101100
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 22/05/2023 21:21:25
+*/
 
---
--- Database: `kos46a`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `idAdmin` int(11) NOT NULL AUTO_INCREMENT,
+  `namaAdmin` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nomorTelepon` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idAdmin`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Struktur dari tabel `admin`
---
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'Gio', '087742037644', 'gio@gmail.com', 'YWRtaW5rdQ==');
+INSERT INTO `admin` VALUES (2, 'Satria', '087742037644', 'satria@gmail.com', 'YWRtaW5rdQ==');
 
-CREATE TABLE `admin` (
-  `idAdmin` int(11) NOT NULL,
-  `namaAdmin` varchar(500) NOT NULL,
-  `nomorTelepon` varchar(500) NOT NULL,
-  `email` varchar(500) NOT NULL,
-  `password` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `admin`
---
-
-INSERT INTO `admin` (`idAdmin`, `namaAdmin`, `nomorTelepon`, `email`, `password`) VALUES
-(1, 'Gio', '087742037644', 'gio@gmail.com', 'YWRtaW5rdQ=='),
-(2, 'Satria', '087742037644', 'satria@gmail.com', 'YWRtaW5rdQ==');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `fasilitas`
---
-
-CREATE TABLE `fasilitas` (
-  `idFasilitas` int(11) NOT NULL,
-  `namaFasilitas` varchar(500) NOT NULL,
+-- ----------------------------
+-- Table structure for fasilitas
+-- ----------------------------
+DROP TABLE IF EXISTS `fasilitas`;
+CREATE TABLE `fasilitas`  (
+  `idFasilitas` int(11) NOT NULL AUTO_INCREMENT,
+  `namaFasilitas` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `hargaFasilitas` double NOT NULL,
-  `fotoFasilitas` varchar(500) NOT NULL,
-  `stok` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `fotoFasilitas` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `stok` int(11) NOT NULL,
+  PRIMARY KEY (`idFasilitas`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data untuk tabel `fasilitas`
---
+-- ----------------------------
+-- Records of fasilitas
+-- ----------------------------
+INSERT INTO `fasilitas` VALUES (1, 'Air Conditioner', 250000, '', 15);
+INSERT INTO `fasilitas` VALUES (2, 'Parkir Motor', 100000, '', 18);
+INSERT INTO `fasilitas` VALUES (3, 'Parkir Mobil', 150000, '', 6);
+INSERT INTO `fasilitas` VALUES (4, 'Televisi', 200000, '', 19);
+INSERT INTO `fasilitas` VALUES (5, 'Laundry', 150000, '', 32);
 
-INSERT INTO `fasilitas` (`idFasilitas`, `namaFasilitas`, `hargaFasilitas`, `fotoFasilitas`, `stok`) VALUES
-(1, 'Air Conditioner', 250000, '', 15),
-(2, 'Parkir Motor', 100000, '', 18),
-(3, 'Parkir Mobil', 150000, '', 6),
-(4, 'Televisi', 200000, '', 19),
-(5, 'Laundry', 150000, '', 32);
+-- ----------------------------
+-- Table structure for kamar
+-- ----------------------------
+DROP TABLE IF EXISTS `kamar`;
+CREATE TABLE `kamar`  (
+  `nomorKamar` int(11) NOT NULL AUTO_INCREMENT,
+  `tipeKamar` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`nomorKamar`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of kamar
+-- ----------------------------
+INSERT INTO `kamar` VALUES (1, 'Small', 'Tersedia');
+INSERT INTO `kamar` VALUES (2, 'Small', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (3, 'Small', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (4, 'Small', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (5, 'Small', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (6, 'Medium', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (7, 'Medium', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (8, 'Medium', 'Tersedia');
+INSERT INTO `kamar` VALUES (9, 'Medium', 'Tersedia');
+INSERT INTO `kamar` VALUES (10, 'Deluxe', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (11, 'Deluxe', 'Tersedia');
+INSERT INTO `kamar` VALUES (12, 'Deluxe', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (13, 'Deluxe', 'Tidak Tersedia');
+INSERT INTO `kamar` VALUES (14, 'Deluxe', 'Tersedia');
+INSERT INTO `kamar` VALUES (15, 'Deluxe', 'Tidak Tersedia');
 
---
--- Struktur dari tabel `kamar`
---
+-- ----------------------------
+-- Table structure for m_tipekamar
+-- ----------------------------
+DROP TABLE IF EXISTS `m_tipekamar`;
+CREATE TABLE `m_tipekamar`  (
+  `idTipeKamar` int(11) NOT NULL AUTO_INCREMENT,
+  `namaTipeKamar` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `thumbnailKamar` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idAdmin` int(11) NULL DEFAULT NULL,
+  `created_at` datetime NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`idTipeKamar`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 52 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `kamar` (
-  `nomorKamar` int(11) NOT NULL,
-  `tipeKamar` varchar(500) NOT NULL,
-  `status` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- ----------------------------
+-- Records of m_tipekamar
+-- ----------------------------
+INSERT INTO `m_tipekamar` VALUES (42, 'Small', '1684502105_header.jpg', 2, '2023-05-19 21:00:55', NULL, NULL);
+INSERT INTO `m_tipekamar` VALUES (43, 'Medium', '1684762813_msg-463887772-4921.jpg', 2, '2023-05-19 21:00:55', '2023-05-22 20:40:13', NULL);
+INSERT INTO `m_tipekamar` VALUES (50, 'Kos', '1684504728_kamarkos.png', 2, '2023-05-19 21:00:55', '2023-05-19 21:01:48', NULL);
+INSERT INTO `m_tipekamar` VALUES (51, 'Royal', '1684762836_Screen Shot 2023-04-14 at 01.11.35.png', 2, '2023-05-22 20:40:36', '2023-05-22 20:40:36', NULL);
 
---
--- Dumping data untuk tabel `kamar`
---
-
-INSERT INTO `kamar` (`nomorKamar`, `tipeKamar`, `status`) VALUES
-(1, 'Small', 'Tersedia'),
-(2, 'Small', 'Tidak Tersedia'),
-(3, 'Small', 'Tidak Tersedia'),
-(4, 'Small', 'Tidak Tersedia'),
-(5, 'Small', 'Tidak Tersedia'),
-(6, 'Medium', 'Tidak Tersedia'),
-(7, 'Medium', 'Tidak Tersedia'),
-(8, 'Medium', 'Tersedia'),
-(9, 'Medium', 'Tersedia'),
-(10, 'Deluxe', 'Tidak Tersedia'),
-(11, 'Deluxe', 'Tersedia'),
-(12, 'Deluxe', 'Tidak Tersedia'),
-(13, 'Deluxe', 'Tidak Tersedia'),
-(14, 'Deluxe', 'Tersedia'),
-(15, 'Deluxe', 'Tidak Tersedia');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `m_tipekamar`
---
-
-CREATE TABLE `m_tipekamar` (
+-- ----------------------------
+-- Table structure for m_tipekamar_pengelolaan
+-- ----------------------------
+DROP TABLE IF EXISTS `m_tipekamar_pengelolaan`;
+CREATE TABLE `m_tipekamar_pengelolaan`  (
+  `idPengelolaan` int(11) NOT NULL AUTO_INCREMENT,
   `idTipeKamar` int(11) NOT NULL,
-  `namaTipeKamar` varchar(500) NOT NULL,
-  `thumbnailKamar` varchar(500) NOT NULL,
-  `idAdmin` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `idAdmin` int(11) NOT NULL,
+  `harga` bigint(100) NULL DEFAULT NULL,
+  `created_at` datetime NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`idPengelolaan`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 58 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data untuk tabel `m_tipekamar`
---
+-- ----------------------------
+-- Records of m_tipekamar_pengelolaan
+-- ----------------------------
+INSERT INTO `m_tipekamar_pengelolaan` VALUES (52, 42, 2, 1000000, '2023-05-22 20:49:45', NULL, NULL);
+INSERT INTO `m_tipekamar_pengelolaan` VALUES (53, 43, 2, 1500000, '2023-05-22 21:02:22', '2023-05-22 21:02:27', NULL);
+INSERT INTO `m_tipekamar_pengelolaan` VALUES (54, 50, 2, 2000000, '2023-05-22 21:02:22', '2023-05-22 21:02:32', NULL);
+INSERT INTO `m_tipekamar_pengelolaan` VALUES (55, 51, 2, 2500000, '2023-05-22 21:02:22', '2023-05-22 21:02:35', NULL);
+INSERT INTO `m_tipekamar_pengelolaan` VALUES (56, 42, 1, 1100000, '2023-05-22 21:03:10', '2023-05-22 21:03:10', NULL);
+INSERT INTO `m_tipekamar_pengelolaan` VALUES (57, 43, 2, 1550000, '2023-05-22 21:02:23', '2023-05-22 21:10:52', NULL);
 
-INSERT INTO `m_tipekamar` (`idTipeKamar`, `namaTipeKamar`, `thumbnailKamar`, `idAdmin`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(42, 'Small', '1684502105_header.jpg', 2, '2023-05-19 21:00:55', NULL, NULL),
-(43, 'Medium', '1684504734_logo46a.png', 1, '2023-05-19 21:00:55', NULL, NULL),
-(50, 'Kos', '1684504728_kamarkos.png', 2, '2023-05-19 21:00:55', '2023-05-19 21:01:48', NULL);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `m_user`
---
-
-CREATE TABLE `m_user` (
-  `idUser` int(11) NOT NULL,
-  `namaUser` varchar(500) NOT NULL,
-  `jenisKelamin` varchar(50) NOT NULL,
+-- ----------------------------
+-- Table structure for m_user
+-- ----------------------------
+DROP TABLE IF EXISTS `m_user`;
+CREATE TABLE `m_user`  (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `namaUser` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `jenisKelamin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `tanggalLahir` date NOT NULL,
-  `nomorTelepon` varchar(50) NOT NULL,
-  `email` varchar(500) NOT NULL,
-  `password` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `nomorTelepon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idUser`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data untuk tabel `m_user`
---
+-- ----------------------------
+-- Records of m_user
+-- ----------------------------
+INSERT INTO `m_user` VALUES (39, 'Satria', 'Pria', '1999-01-20', '083832204284', 'satria@gmail.com', 'MTIzNDU2Nzg5');
+INSERT INTO `m_user` VALUES (41, 'Satria', 'Pria', '1999-01-20', '083832204284', 'satriags@gmail.com', 'MTIzNDU2Nzg5');
+INSERT INTO `m_user` VALUES (42, 'Udin', 'Pria', '1999-01-20', '083838383883', 'yahi@gmail.com', 'MTIzNDU2Nzg5');
+INSERT INTO `m_user` VALUES (43, 'yoooo', 'Pria', '0002-02-22', '2222222', 'yahi2@gmail.com', 'MTIzNDU2Nzg5');
+INSERT INTO `m_user` VALUES (44, 'Giovanni', 'Pria', '2001-12-31', '083838388383', 'gio@gmail.com', 'MTIz');
 
-INSERT INTO `m_user` (`idUser`, `namaUser`, `jenisKelamin`, `tanggalLahir`, `nomorTelepon`, `email`, `password`) VALUES
-(39, 'Satria', 'Pria', '1999-01-20', '083832204284', 'satria@gmail.com', 'MTIzNDU2Nzg5'),
-(41, 'Satria', 'Pria', '1999-01-20', '083832204284', 'satriags@gmail.com', 'MTIzNDU2Nzg5'),
-(42, 'Udin', 'Pria', '1999-01-20', '083838383883', 'yahi@gmail.com', 'MTIzNDU2Nzg5'),
-(43, 'yoooo', 'Pria', '0002-02-22', '2222222', 'yahi2@gmail.com', 'MTIzNDU2Nzg5'),
-(44, 'Giovanni', 'Pria', '2001-12-31', '083838388383', 'gio@gmail.com', 'MTIz');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `transaksi`
---
-
-CREATE TABLE `transaksi` (
-  `idTransaksi` int(11) NOT NULL,
+-- ----------------------------
+-- Table structure for transaksi
+-- ----------------------------
+DROP TABLE IF EXISTS `transaksi`;
+CREATE TABLE `transaksi`  (
+  `idTransaksi` int(11) NOT NULL AUTO_INCREMENT,
   `tanggalTransaksi` date NOT NULL,
   `waktuTransaksi` time NOT NULL,
   `lamaSewa` int(11) NOT NULL,
-  `buktiPembayaran` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `buktiPembayaran` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idTransaksi`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Indexes for dumped tables
---
+-- ----------------------------
+-- Records of transaksi
+-- ----------------------------
 
---
--- Indeks untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`idAdmin`);
-
---
--- Indeks untuk tabel `fasilitas`
---
-ALTER TABLE `fasilitas`
-  ADD PRIMARY KEY (`idFasilitas`);
-
---
--- Indeks untuk tabel `kamar`
---
-ALTER TABLE `kamar`
-  ADD PRIMARY KEY (`nomorKamar`);
-
---
--- Indeks untuk tabel `m_tipekamar`
---
-ALTER TABLE `m_tipekamar`
-  ADD PRIMARY KEY (`idTipeKamar`);
-
---
--- Indeks untuk tabel `m_user`
---
-ALTER TABLE `m_user`
-  ADD PRIMARY KEY (`idUser`);
-
---
--- Indeks untuk tabel `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`idTransaksi`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `admin`
---
-ALTER TABLE `admin`
-  MODIFY `idAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `fasilitas`
---
-ALTER TABLE `fasilitas`
-  MODIFY `idFasilitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT untuk tabel `kamar`
---
-ALTER TABLE `kamar`
-  MODIFY `nomorKamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT untuk tabel `m_tipekamar`
---
-ALTER TABLE `m_tipekamar`
-  MODIFY `idTipeKamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
-
---
--- AUTO_INCREMENT untuk tabel `m_user`
---
-ALTER TABLE `m_user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-
---
--- AUTO_INCREMENT untuk tabel `transaksi`
---
-ALTER TABLE `transaksi`
-  MODIFY `idTransaksi` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
